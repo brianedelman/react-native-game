35352d2b03c4e0f80cb87c01b7aa0de17f39d2f1
Brian Edelman <bd.edelman@gmail.com>
2020-08-21 15:30:45 -0400

    adds redux

--- App.js
+++ App.js
@@ -1,13 +1,19 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { Provider } from 'react-redux';
+import configureStore from './src/redux/store';
+
+const store = configureStore();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>hi</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Provider store={store}>
+      <View style={styles.container}>
+        <Text>hi</Text>
+        <StatusBar style="auto" />
+      </View>
+    </Provider>
   );
 }
 
